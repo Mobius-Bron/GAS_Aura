b1b386bd68aee62cf12f69630cb70257c7de0d82
Mobius-Bron <1436129108@qq.com>
2026-04-10 22:07:14 +0800

    修复药水瓶问题

--- Plugins/GasTemplate/Source/GasTemplate/Private/Actor/VirgoEffectActor.cpp
+++ Plugins/GasTemplate/Source/GasTemplate/Private/Actor/VirgoEffectActor.cpp
@@ -4,6 +4,11 @@
 #include "Actor/VirgoEffectActor.h"
 #include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "AbilitySystemInterface.h"
+
+#include "AbilitySystem/VirgoAbilitySystemComponent.h"
+#include "AbilitySystem/VirgoAttributeSet.h"
+#include "Character/VirgoHeroCharacter.h"
 
 // Sets default values
 AVirgoEffectActor::AVirgoEffectActor()
@@ -11,12 +16,41 @@ AVirgoEffectActor::AVirgoEffectActor()
 	PrimaryActorTick.bCanEverTick = false;
 
 	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SetRootComponent(SphereComponent);
+
 	ActorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ActorMesh"));
+	ActorMesh->SetupAttachment(SphereComponent);
+	ActorMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AVirgoEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ActorGameplayEffect == nullptr) { return; }
+
+	if(AVirgoHeroCharacter* VirgoHeroCharacter = Cast<AVirgoHeroCharacter>(OtherActor))
+	{
+		if (IAbilitySystemInterface* ASInterface = Cast<IAbilitySystemInterface>(OtherActor))
+		{
+			UGameplayEffect* EffectCDO = ActorGameplayEffect->GetDefaultObject<UGameplayEffect>();
+			ASInterface->GetAbilitySystemComponent()->ApplyGameplayEffectToSelf(
+				EffectCDO,
+				1,
+				ASInterface->GetAbilitySystemComponent()->MakeEffectContext()
+			);
+		}
+		Destroy();
+	}
+}
+
+void AVirgoEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
 
 void AVirgoEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnOverlap);
+	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &ThisClass::EndOverlap);
 }
 
--- Plugins/GasTemplate/Source/GasTemplate/Public/Actor/VirgoEffectActor.h
+++ Plugins/GasTemplate/Source/GasTemplate/Public/Actor/VirgoEffectActor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayEffect.h"
 #include "VirgoEffectActor.generated.h"
 
 class USphereComponent;
@@ -16,6 +17,15 @@ class GASTEMPLATE_API AVirgoEffectActor : public AActor
 	
 public:	
 	AVirgoEffectActor();
+
+	UFUNCTION()
+	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> ActorGameplayEffect;
 
 protected:
 	virtual void BeginPlay() override;
